b71c060b704e29a44cd68cd72d36f29a1879a3b1
Pavicevic Tea <pavicevictea6@gmail.com>
2026-01-23 15:56:32 +0100

    feat: new model class added

---
+++ src/main/java/com/isa/backend/model/VideoLike.java
@@ -0,0 +1,40 @@
+package com.isa.backend.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "VIDEO_LIKES", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "video_id"})
+})
+public class VideoLike implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "video_id", nullable = false)
+    private VideoPost video;
+
+    public VideoLike() {}
+
+    public VideoLike(User user, VideoPost video){
+        this.user = user;
+        this.video = video;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public VideoPost getVideo() { return video; }
+    public void setVideo(VideoPost video) { this.video = video; }
+}
